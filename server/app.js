0ba5387b99f5f408cfd8542e6d769c14d4621d92
hrawat0308 <hrawat0308@gmail.com>
2025-02-04 11:56:37 +0530

    updated mongo connection

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser= require('body-parser');
 const mongoose = require('mongoose');
 const postsRoutes = require('./routes/posts');
 const usersRoutes = require('./routes/users');
+require('dotenv').config();
 
 const app = express();
 
@@ -32,10 +33,21 @@ app.use((error,req,res,next)=>{
     res.json({ message : error.message || "An Unknown Error Occured"});
 });
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qjfxp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-        .then(()=>{
-            app.listen( process.env.PORT || 5000);
-        })
-        .catch(err=>{
-            console.log(err);
-        });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qjfxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+app.listen(process.env.PORT || 5000 , () => {
+    console.log("app running on port", process.env.PORT || 5000)
+});
